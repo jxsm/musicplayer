35cebb3550063a1177e35fe724674207145adbaa
jxsm <jxsmbilibili@qq.com>
2023-12-25 15:40:26 +0800

    窗口顶部的控制条

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--窗口顶部控制条-->
+    <controlStrip></controlStrip>
+    
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import controlStrip from "./components/controlStrip.vue"//窗口顶部的控制条
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components:{
+        controlStrip
+    },
+    data(){
+        return{
+            
+        }
+    },
+    methods:{
+      
+    },
+    
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+let app =  createApp(App)
+
+
+app.mount('#app')
